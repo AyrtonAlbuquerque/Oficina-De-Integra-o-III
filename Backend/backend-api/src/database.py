b0abec090f430521a5582231fb31a0d204270cc8
ian.queros <ian@4vants.com.br>
2022-05-07 10:18:32 -0300

    Create config and database tables

--- Backend/backend-api/src/database.py
+++ Backend/backend-api/src/database.py
@@ -1,5 +1,6 @@
 import psycopg2
 from contextlib import contextmanager
+from src.config import LOAD_TEST_DATA
 
 DB_PASSWORD = 'admin'
 
@@ -13,7 +14,6 @@ connectionArgs = {
 
 def setup():
   with get_cursor() as (conn, cur):
-    # Execute a command: this creates a new table
     cur.execute("""CREATE TABLE IF NOT EXISTS users (
       username varchar PRIMARY KEY,
       pwd_hash varchar,
@@ -27,32 +27,39 @@ def setup():
         from_account varchar,
         amount float(4),
         created_at date);""")
+    
+    cur.execute("""
+      CREATE TABLE IF NOT EXISTS recyclers (
+        ID serial PRIMARY KEY,
+        name varchar,
+        description varchar);""")
+
+    cur.execute("""
+      CREATE TABLE IF NOT EXISTS classifications (
+        ID serial PRIMARY KEY,
+        recycler_id int,
+        label int,
+        redeem_code varchar,
+        is_redeemed boolean,
+        image_path varchar,
+        CONSTRAINT recycler_cons FOREIGN KEY(recycler_id)  REFERENCES recyclers(ID));""")
+
+    if LOAD_TEST_DATA:
+      cur.execute("SELECT * FROM recyclers WHERE name='TestRecycler'")
+      if not cur.fetchone():
+        cur.execute("""
+          INSERT INTO recyclers (name, description)
+            VALUES (%s, %s);
+          """, ('TestRecycler', 'Recycler to test'))
+        conn.commit()
+      
 
 @contextmanager
 def get_cursor():
+  """Returns database connection and cursor as a tuple"""
   with psycopg2.connect(**connectionArgs) as conn:
     with conn.cursor() as cur:
       yield (conn, cur)
-
-
-def other():
-
-  # Pass data to fill a query placeholders and let Psycopg perform
-  # the correct conversion (no more SQL injections!)
-  cur.execute("INSERT INTO User (num, data) VALUES (%s, %s)", (101, "abc'def"))
-  cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (102, "abc'def"))
-  cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (103, "abc'def"))
-
-  # Query the database and obtain data as Python objects
-  cur.execute("SELECT * FROM test;")
-  print(cur.fetchone())
-  print(cur.fetchone())
-  print(cur.fetchone())
-  print(cur.fetchone())
-
-  # Make the changes to the database persistent
-  conn.commit()
-
 
 if __name__ == '__main__':
   setup()
