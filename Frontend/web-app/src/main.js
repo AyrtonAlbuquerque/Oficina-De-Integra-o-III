9adad3f8f8dde1c2afb213c0b29d99b473bd9c3b
ian.queros <ian@4vants.com.br>
2022-05-08 01:46:16 -0300

    Add axios interceptor to authorize user

--- Frontend/web-app/src/main.js
+++ Frontend/web-app/src/main.js
@@ -3,10 +3,12 @@ import VueCookies from 'vue-cookies';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import alert from './plugins/alert';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueCookies);
+Vue.use(alert);
 
 new Vue({
   router,
--- Frontend/web-app/src/plugins/axios.js
+++ Frontend/web-app/src/plugins/axios.js
@@ -4,16 +4,26 @@ import router from '../router';
 
 const { default: axios } = Axios;
 
+const backendUrl = 'http://localhost:8081';
+
+axios.defaults.baseURL = backendUrl;
+
 axios.interceptors.request.use(
-  async (res) => res,
+  async (res) => {
+    const authToken = Vue.$cookies.get('authentication-token');
+    if (authToken) {
+      res.headers['Authorization'] = `Bearer ${authToken}`;
+    }
+    return res
+  },
   async (err) => {
     const status = err.response ? err.response.status : null;
     const url = err.request ? err.request.responseURL : null;
 
-    if (url && (url.endsWith('/auth/token') || url.endsWith('/auth/revoke'))) {
+    if (url && (url.endsWith('/user/signin') || url.endsWith('/user/signup'))) {
       throw err;
     } else if (status === 401) {
-      Vue.$cookies.remove('authenticationToken');
+      Vue.$cookies.remove('authentication-token');
       router.push({ name: 'Sign in' });
     }
   },
